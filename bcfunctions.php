79886b21bcc7550b04a37e029e014ff44e83ab75
haya94 <haya.kazmi@mail.utoronto.ca>
2016-03-24 06:28:16 -0400

    functions

--- bcfunctions.php
+++ bcfunctions.php
@@ -2,4 +2,8 @@
 
 // This calls the file that makes our basic cafe widget for social media
 require get_stylesheet_directory() . '/basic-cafe-social-links-widget.php';
+
+// This calls the file that makes our recent post widget
+require get_stylesheet_directory() . '/postwidget.php';
+
 ?>
